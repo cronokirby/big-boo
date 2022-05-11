ec5ab1ae0b505b906066fc572c2a8fd514aefadc
Lúcás Críostóir Meier <lucas@cronokirby.com>
2022-05-12 00:39:34 +0200

    Define Operation type
    This will represent the individual operations in our bytecode.
    
    Starting with just a very bare-bones set for now. We'll expand this
    later, but the goal is to get end-to-end testing of this as soon as
    possible.

--- boo-bc/src/lib.rs
+++ boo-bc/src/lib.rs
@@ -1,8 +1,24 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+use std::fmt;
+
+/// Represents a single operation in our bytecode.
+///
+/// Each of the operations manipulates values on the stack, potentially performing
+/// some kind of bitwise operation or other arithmetic on those values.
+#[derive(Clone, Debug, PartialEq)]
+enum Operation {
+    /// Xor the top two elements of the stack, replacing them.
+    Xor,
+    /// And the top two elements of the stack, replacing them.
+    And,
+}
+
+impl fmt::Display for Operation {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        use Operation::*;
+
+        match self {
+            Xor => write!(f, "Xor"),
+            And => write!(f, "And"),
+        }
     }
 }
